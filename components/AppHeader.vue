3dda6260ab0be2c69eb358f6f5fff46d19d02352
Willo0ow <dhinc39@gmail.com>
2023-11-08 21:40:57 +0100

    scaffolding

---
+++ components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <h1>My App</h1>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+    </nav>
+  </header>
+</template>
+<script setup lang="ts"></script>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+  </div>
+</template>
+<script setup lang="ts"></script>
