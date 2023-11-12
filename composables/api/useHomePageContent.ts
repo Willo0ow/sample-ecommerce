00ecc8343b0ae0af0ae878ccfbd561bd30e74f35
Willo0ow <dhinc39@gmail.com>
2023-11-12 09:52:28 +0100

    home page hero

--- composables/api/useHomePageContent.ts
+++ composables/api/useHomePageContent.ts
@@ -10,6 +10,8 @@ export const useHomePageContent = async <T>(slug: string) => {
     const response = await findOne(slug, {
       populate: {
         sections: { populate: "*" },
+        route: { populate: "*" },
+        heroImage: { populate: "*" },
       },
     });
     return response.data as T;
