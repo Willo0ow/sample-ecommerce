6bcd8e30e8954427b0032833e4841e42a9039d42
Willo0ow <dhinc39@gmail.com>
2023-11-12 15:49:06 +0100

    fix retrieving home page content

--- composables/api/useHomePageContent.ts
+++ composables/api/useHomePageContent.ts
@@ -1,20 +1,16 @@
-export const useHomePageContent = async <T>(slug: string) => {
-  const story = useState(slug);
+export const useHomePageContent = async <T>() => {
+  const story = useState("home-page");
 
   const {
     data: content,
     pending,
     refresh,
-  } = await useAsyncData<T>(slug, async () => {
-    const { findOne } = useStrapi();
-    const response = await findOne(slug, {
-      populate: {
-        sections: { populate: "*" },
-        route: { populate: "*" },
-        heroImage: { populate: "*" },
-      },
-    });
-    return response.data as T;
+  } = await useAsyncData<T>("home-page", async () => {
+    const response = await $fetch<T>(
+      "http://localhost:1337/api/home-page?populate[heroImage]=%2A&populate[sections][on][common.testimonials][populate]=%2A&populate[sections][on][common.grid-of-cards][populate]=%2A"
+    );
+
+    return response.data;
   });
 
   if (!story.value) {
