527ddc4c516bae73f91af85d79310bf3de7e6595
Willo0ow <dhinc39@gmail.com>
2023-11-18 13:39:57 +0100

    Expandable section

--- components/SingleTestimonial.vue
+++ components/SingleTestimonial.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="text-center px-10 md:px-40">
-    <p class="text-[2.8rem] md:text-[3.8rem] leading-[1.5] font-light">
+    <p class="text-[2rem] md:text-[3.8rem] leading-[1.5] font-light">
       "{{ testimonial.description }}"
     </p>
-    <p class="text-[2rem] mt-16">{{ testimonial.name }}</p>
-    <p class="text-[2rem] mt-2 font-thin">{{ testimonial.position }}</p>
+    <p class="text-[1.8rem] md:text-[2rem] mt-16">{{ testimonial.name }}</p>
+    <p class="text-[1.8rem] md:text-[2rem] mt-2 font-thin">
+      {{ testimonial.position }}
+    </p>
   </div>
 </template>
 <script setup lang="ts">
