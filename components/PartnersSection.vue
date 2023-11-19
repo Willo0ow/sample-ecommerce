89e41c569517c538568354b42c8a44ac71690d20
Willo0ow <dhinc39@gmail.com>
2023-11-19 09:23:48 +0100

    partners section component

---
+++ components/PartnersSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="py-24 md:py-48 px-12 md:px-20">
+    <div class="centered-max-width">
+      <h2 class="text-6xl md:text-8xl mb-12 md:mb-24">{{ section.title }}</h2>
+      <div
+        class="grid grid-cols-3 min-[420px]:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-4"
+      >
+        <img
+          width="70"
+          height="70"
+          class="mx-auto"
+          v-for="logo in section.logos.data"
+          :src="getImageSrc(logo.attributes.url)"
+          :key="logo.id"
+          alt="text"
+        />
+      </div>
+      <nuxt-link
+        :to="section.bottomLink.path ||  section.bottomLink.route.data.attributes.path"
+        class="block mt-12 md:mt-24 text-center uppercase font-semibold text-3xl"
+        >{{ section.bottomLink.label }}</nuxt-link
+      >
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+import type { CommonPartnersLogos } from "~/types/generated";
+defineProps<{
+  section: CommonPartnersLogos["attributes"];
+}>();
+const getImageSrc = (imageUrl: string) => {
+  return "http://localhost:1337" + imageUrl;
+};
+</script>
+<style>
+.swiper-wrapper {
+  align-items: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,13 +17,16 @@
 import GridOfCards from "@/components/GridOfCards.vue";
 import TestimonialSlider from "~/components/TestimonialSlider.vue";
 import Expandable from "~/components/Expandable.vue";
+import PartnersSection from "~/components/PartnersSection.vue";
 import { type ApiHomePageHomePage } from "@/types/generated";
+
 const { content } = await useHomePageContent<ApiHomePageHomePage>();
 
 const getSectionComponent = (strapiComponent: string) => {
   if (strapiComponent === "common.grid-of-cards") return GridOfCards;
   if (strapiComponent === "common.testimonials") return TestimonialSlider;
   if (strapiComponent === "common.expandable") return Expandable;
+  if (strapiComponent === "common.partners-logos") return PartnersSection;
 };
 </script>
 ń
